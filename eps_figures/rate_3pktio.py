c51e51d19bd9c1fed43b60310293f1436af68622
osboxes.org <zjuan22@gmail.com>
2018-07-28 16:36:30 -0400

    grafica con 4 barraas

--- eps_figures/rate_3pktio.py
+++ eps_figures/rate_3pktio.py
@@ -10,12 +10,13 @@ plt.rcParams['axes.axisbelow'] = True
 plt.rcParams['axes.axisbelow'] = True
 
 
-cores =     ["","128","256","512","1024","1280","1518","","","128","256","512","1024","1280","1518","","","128","256","512","1024","1280","1518",]
+cores =     ["","128","","256","","512","","1024","","1280","","1518","","","128","","256","","512","","1024","","1280","","1518","","","128","","256","","512","","1024","","1280","","1518",""]
 
-l2 =      [np.nan ,832,902,1632,3220,3765,4230, np.nan,np.nan,1453,2836,5244,8767,9846,9865, np.nan,np.nan,6900,9575,9802,9809,9846,9857]
-l3 =      [np.nan ,797,818,1690,3290,3820,4126, np.nan,np.nan,1453,2835,5231,8756,9846,9860, np.nan,np.nan,5530,9624,9808,9808,9846,9857]
-nat_ul=   [np.nan ,523,712,1428,2534,3345,3780, np.nan,np.nan,1215,2495,4938,8740,9843,9861, np.nan,np.nan,4000,9240,9620,9809,9846,9857]
-nat_dl=   [np.nan ,512,799,1425,2556,3180,3670, np.nan,np.nan,1230,2450,4938,8738,9846,9860, np.nan,np.nan,4273,9273,9801,9841,9871,9875]
+l2 =      [np.nan , np.nan ,832, np.nan, 902,np.nan,1632,np.nan,3220,np.nan,3765,np.nan,4230, np.nan,np.nan,1453,np.nan,2836,np.nan,5244,np.nan,8767,np.nan,9846,np.nan,9865, np.nan,np.nan,6900,np.nan,9575,np.nan,9802,np.nan,9809,np.nan,9846,np.nan,9857]
+l3 =      [np.nan , np.nan ,797, np.nan, 818,np.nan,1690,np.nan,3290,np.nan,3820,np.nan,4126, np.nan,np.nan,1453,np.nan,2835,np.nan,5231,np.nan,8756,np.nan,9846,np.nan,9860, np.nan,np.nan,5530,np.nan,9624,np.nan,9808,np.nan,9808,np.nan,9846,np.nan,9857]
+
+nat_ul=   [np.nan , 523, np.nan, 712,np.nan,1428,np.nan,2534,np.nan,3345,np.nan,3780, np.nan,np.nan,1215,np.nan,2495,np.nan,4938,np.nan,8740,np.nan,9843,np.nan,9861, np.nan,np.nan,4000,np.nan,9240,np.nan,9620,np.nan,9809,np.nan,9846,np.nan,9857,np.nan]
+nat_dl=   [np.nan , 512, np.nan, 799,np.nan,1425,np.nan,2556,np.nan,3180,np.nan,3670, np.nan,np.nan,1230,np.nan,2450,np.nan,4938,np.nan,8738,np.nan,9846,np.nan,9860, np.nan,np.nan,4273,np.nan,9273,np.nan,9801,np.nan,9841,np.nan,9871,np.nan,9875,np.nan]
 
 #dpdk_p = dpdk
 #netmap_p = netmap
@@ -38,10 +39,10 @@ fig = plt.figure()
 ax = fig.add_subplot(111)
 #ax.bar([p + width for p in ind], dpdk_p, width, color="#AFD2E9", edgecolor="black")
 #ax.bar([p + width for p in ind+0.3], dpdk_p, width, edgecolor="black",hatch="OO", lw=1., zorder = 0)
-ax.bar([p + width for p in ind+0.35], l2, width, color= "yellow", edgecolor="black",hatch="OO", lw=1., zorder = 0)
-ax.bar([p + width for p in ind+0.15], l3, width, color= "purple", edgecolor="black",hatch="OO", lw=1., zorder = 0)
-ax.bar([p + width for p in ind-0.15], nat_ul, width, color= "blue", edgecolor="black",hatch="OO", lw=1., zorder = 0)
-ax.bar([p + width for p in ind-0.35], nat_dl, width, color= "red", edgecolor="black",hatch="OO", lw=1., zorder = 0)
+ax.bar([p + width for p in ind+0], l2, width, color= "yellow", edgecolor="black",hatch="OO", lw=1., zorder = 0)
+ax.bar([p + width for p in ind-0.3], l3, width, color= "purple", edgecolor="black",hatch="OO", lw=1., zorder = 0)
+ax.bar([p + width for p in ind], nat_ul, width, color= "blue", edgecolor="black",hatch="OO", lw=1., zorder = 0)
+ax.bar([p + width for p in ind+0.3], nat_dl, width, color= "red", edgecolor="black",hatch="OO", lw=1., zorder = 0)
 
 #ax.bar([p + width for p in ind], netmap_p, width, edgecolor="black", lw=1., zorder = 0)
 
