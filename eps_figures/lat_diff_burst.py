8da30b7e803dd024a211c61d517fd2785c2abd55
zjuan22 <zjuan22@gmail.com>
2018-07-30 02:50:40 +0000

    added diff burst figures

---
+++ eps_figures/lat_diff_burst.py
@@ -0,0 +1,86 @@
+#import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from cycler import cycler
+
+plt.rcParams['axes.spines.right'] = True 
+plt.rcParams['axes.spines.bottom'] = True
+plt.rcParams['axes.spines.left'] = True 
+plt.rcParams['axes.axisbelow'] = True
+plt.rcParams['axes.axisbelow'] = True
+plt.rcParams['figure.figsize'] = 7, 3.5 
+
+
+markers = ["*","o","^",".",">"]
+
+x_values= ["128","256","512","1024","1280","1518"]
+
+l2_1_netmap=  [8.5586,8.8094,9.0052,9.9238,10.5094,10.8576]
+l2_32_netmap= [13.0864,13.2946,13.7014,14.3362,14.8114,15.4322]
+l2_128_netmap=[13.5354,13.5466,13.7292,14.337,14.7586,15.3154]
+l2_256_netmap=[13.0148,13.5952,13.7086,14.4354,14.7808,15.5314]
+
+l2_16_dpdk=   [5.3586,5.5536,6.1406,6.6972,6.9564,7.5056]
+l2_32_dpdk=   [5.3232,5.6058,5.9798,6.6484,6.9806,7.6048]
+
+
+ind = np.arange(len(x_values))
+
+width = 0.3
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_ylim([0, 16])
+
+
+dashList = [(4,2,20,2),(2,2),(5,1),(4,3,2,2),(8,4,3,1)] 
+verde_oscuro="#006633"
+azul_cielo="#2248E1"
+
+fucsia="#DA22E1"
+cafe="#A91524"
+
+azul_mar="#161673"
+azul_claro="#1AE2E2"
+
+tomate="#E88411"
+verde_fosfo="#B2E835"
+
+ax.yaxis.grid(color='gray' )
+
+ax.plot(ind, l2_1_netmap,  marker=markers[4],  color="green",  dashes=dashList[4],markerfacecolor="green",markeredgecolor="green", lw=2, markersize=9 )
+ax.plot(ind, l2_32_netmap,  marker=markers[0],  color="green",  dashes=dashList[0],markerfacecolor="green",markeredgecolor="green", lw=2, markersize=9 )
+ax.plot(ind, l2_128_netmap, color="green",  dashes=dashList[1], lw=2, markerfacecolor="green",markeredgecolor="green",  marker=markers[1] )
+ax.plot(ind, l2_256_netmap, color="green",  dashes=dashList[2], lw=2, markerfacecolor="green",markeredgecolor="green",  marker=markers[2] )
+
+ax.plot(ind, l2_16_dpdk,  color="blue",   dashes=dashList[3], lw=2, markerfacecolor="blue",markeredgecolor="blue",  marker=markers[3] )
+ax.plot(ind, l2_32_dpdk,  color="blue",   dashes=dashList[0], lw=2 ,markerfacecolor="blue",markeredgecolor="blue",  marker=markers[0], markersize=9 )
+
+#ax.set_xticks(ind+6*(width/2))
+#xticks = ax.xaxis.get_major_ticks()
+
+ax.set_xticklabels(x_values, fontsize=10)
+
+ax.set_ylabel('Average latency ($\mu$s)',fontsize=10)
+#ax.yaxis.set_label_coords(-0.08,0.9)
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.yaxis.set_ticks_position('left')
+#ax.tick_params(axis='y',length=6)
+ax.xaxis.set_ticks_position('bottom')
+ax.set_xlabel("Packet size (Bytes)", fontsize=9)
+ax.xaxis.set_label_coords(0.5,-0.2)
+ax.yaxis.grid(linestyle='--')
+
+#ax.text(5, -1.2, u'Socket-mmap',fontsize=10)
+#ax.text(19, -1.2, u'Netmap',fontsize=10)
+#ax.text(33, -1.2, u'DPDK',fontsize=10)
+#ax.set_ylim([0,7.4])
+
+#plt.legend(['1 Pkt Burst-Netmap','32 Pkt Burst-Netmap','128 Pkt Burst-Netmap','256 Pkt Burst-Netmap','16 Pkt Burst-DPDK','32 Pkt Burst-DPDK'], loc='center right',frameon=False, fontsize= 11)
+plt.legend(['1 Pkt Burst-Netmap','32 Pkt Burst-Netmap','128 Pkt Burst-Netmap','256 Pkt Burst-Netmap','16 Pkt Burst-DPDK','32 Pkt Burst-DPDK'], bbox_to_anchor=(0., 0.05, 1., .202), loc=3, ncol=3, mode="expand", borderaxespad=0., frameon=False, fontsize= 9)
+plt.tight_layout(pad=0.3, w_pad=0.5, h_pad=1)
+
+filename="lat_diff_burst"
+plt.savefig(filename+".eps")
+plt.savefig(filename+".png")
+plt.savefig(filename+".svg")
---
+++ eps_figures/rate_diff_burs_netmap.py
@@ -0,0 +1,85 @@
+#import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from cycler import cycler
+
+plt.rcParams['axes.spines.right'] = True 
+plt.rcParams['axes.spines.bottom'] = True
+plt.rcParams['axes.spines.left'] = True 
+plt.rcParams['axes.axisbelow'] = True
+plt.rcParams['axes.axisbelow'] = True
+plt.rcParams['figure.figsize'] = 6.5, 3.5 
+
+
+markers = ["*","o","^","."]
+
+x_values= ["128","256","512","1024","1280","1518"]
+
+l2_32_netmap= [1.453,2.836,5.244,8.767,9.846,9.865]
+l2_128_netmap=[2.343,4.5,7.94,9.829,9.855,9.865]
+l2_256_netmap=[2.732,5.23,8.92,9.82,9.856,9.86]
+l2_16_dpdk=   [7.15,9.275,9.624,9.809,9.843,9.852]
+l2_32_dpdk=   [6.9,9.269,9.82,9.809,9.846,9.857]
+
+
+ind = np.arange(len(x_values))
+
+width = 0.3
+fig = plt.figure()
+ax = fig.add_subplot(111)
+#ax.set_ylim([0, 10])
+
+
+linestyles = ['-', '--', '-.', ':']
+dashList = [(4,2,20,2),(2,2),(5,1),(4,3,2,2),(8,4,3,1)] 
+verde_oscuro="#006633"
+azul_cielo="#2248E1"
+
+fucsia="#DA22E1"
+cafe="#A91524"
+
+azul_mar="#161673"
+azul_claro="#1AE2E2"
+
+tomate="#E88411"
+verde_fosfo="#B2E835"
+
+ax.yaxis.grid(color='gray' )
+
+ax.plot(ind, l2_32_netmap,  marker=markers[0],  color="green",  dashes=dashList[0],markerfacecolor="green",markeredgecolor="green", lw=2, markersize=9 )
+ax.plot(ind, l2_128_netmap, color="green",  dashes=dashList[1], lw=2, markerfacecolor="green",markeredgecolor="green",  marker=markers[1] )
+ax.plot(ind, l2_256_netmap, color="green",  dashes=dashList[2], lw=2, markerfacecolor="green",markeredgecolor="green",  marker=markers[2] )
+ax.plot(ind, l2_16_dpdk,  color="blue",   dashes=dashList[3], lw=2, markerfacecolor="blue",markeredgecolor="blue",  marker=markers[3] )
+ax.plot(ind, l2_32_dpdk,  color="blue",   dashes=dashList[0], lw=2 ,markerfacecolor="blue",markeredgecolor="blue",  marker=markers[0], markersize=9 )
+
+#ax.set_xticks(ind+6*(width/2))
+#xticks = ax.xaxis.get_major_ticks()
+
+ax.set_xticklabels(x_values, fontsize=10)
+
+ax.set_ylabel('Throughput (Gbps)',fontsize=9)
+#ax.yaxis.set_label_coords(-0.08,0.9)
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.yaxis.set_ticks_position('left')
+#ax.tick_params(axis='y',length=6)
+ax.xaxis.set_ticks_position('bottom')
+ax.set_xlabel("Packet size (Bytes)", fontsize=9)
+ax.xaxis.set_label_coords(0.5,-0.2)
+ax.yaxis.grid(linestyle='--')
+
+#ax.text(5, -1.2, u'Socket-mmap',fontsize=10)
+#ax.text(19, -1.2, u'Netmap',fontsize=10)
+#ax.text(33, -1.2, u'DPDK',fontsize=10)
+#ax.set_ylim([0,7.4])
+
+plt.legend(['32 Pkt Burst-Netmap','128 Pkt Burst-Netmap','256 Pkt Burst-Netmap','16 Pkt Burst-DPDK','32 Pkt Burst-DPDK'], loc='center right',frameon=False, fontsize= 11)
+
+
+plt.tight_layout(pad=0.3, w_pad=0.5, h_pad=1)
+
+filename="rate_diff_burst"
+#plt.savefig("vxlan.png")
+plt.savefig(filename+".eps")
+plt.savefig(filename+".png")
+#plt.savefig(filename+".svg")
